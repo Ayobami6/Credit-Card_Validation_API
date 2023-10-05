8a19e98f23c51486d910d4d4a4a9b813a86a4f3b
Ayobami Alaran A. <ayobamidele006@gmail.com>
2023-10-05 21:30:04 +0000

    Setup dev concurrent start up

--- server/dist/index.js
+++ server/dist/index.js
@@ -10,4 +10,3 @@ app.use('/', router);
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-export default app;
--- server/index.ts
+++ server/index.ts
@@ -14,5 +14,3 @@ app.use('/', router);
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
-export default app;
